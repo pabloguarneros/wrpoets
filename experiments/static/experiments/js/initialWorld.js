39daba039043a6558bc549dd7d8955ae09dfcf31
pabloguarneros <wrno.pablo@gmail.com>
2021-07-06 14:27:14 -0500

    Changed the style for each poetry box by adding a border color for each poem to pop and by making it scrollable for longer verses

--- experiments/static/experiments/js/initialWorld.js
+++ experiments/static/experiments/js/initialWorld.js
@@ -1,6 +1,5 @@
 import { ARButton } from 'https://unpkg.com/three@0.126.0/examples/jsm/webxr/ARButton.js';
 
-
 $(document).ready(function(){
     
     let scene, clock, camera, renderer, raycaster, markerData;
@@ -19,34 +18,34 @@ $(document).ready(function(){
         clock = new THREE.Clock();
         renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         renderer.setSize(window.innerWidth, window.innerHeight);
+        
         scene.fog = new THREE.FogExp2( 0x47297B, .6 );
         raycaster = new THREE.Raycaster();
     
         var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         light.position.set(0.5, 1, 0.25);
         scene.add(light);
-    
-        markerData = [];
+
+        loadOrbs();
     
         function loadOrbs(){
 
-            const loadingManager = new THREE.LoadingManager(
-                // Loaded
-                () =>
-                {window.setTimeout(() =>
-                    {}, 500)
-                    window.setTimeout(() =>
-                    { curtains_off();}, 2000)
-                },
-                (itemUrl, itemsLoaded, itemsTotal) =>
-                { const progressRatio = itemsLoaded / itemsTotal }
-            )        
-    
+            const loadingManager = new THREE.LoadingManager( () => {
+                window.setTimeout(() => { reveal() }, 50)
+            })        
+
+            function reveal() {
+                $('#home_loader').css("animation","curtain_off 1s ease 1s 1 forwards")
+                $('#home_loader').on("animationend",function(e) {
+                    $('#home_loader').css("display","none");
+                })
+            }
+
             const ar_display = new THREE.PlaneBufferGeometry(.3,.3);
             const defaultGeometry = new THREE.SphereBufferGeometry(.14,40,40);
-            const defaultMaterial = new THREE.MeshPhongMaterial({
-            color      :  new THREE.Color(0xF5BD1F),
-                });
+            const defaultMaterial = new THREE.MeshPhongMaterial( { color : new THREE.Color(0xF5BD1F) });
+
+            markerData = [];
         
             for (var int in nodeArray){
     
@@ -54,12 +53,10 @@ $(document).ready(function(){
                 const x_origin = node['x_position']
                 const y_origin = node['y_position']
                 const z_origin = node['z_position']
-                const squishy = node['squishy']
-                const pk = node['pk']
                 const images = node['images']
         
                 markerData.push({
-                    position : [node['x_position'],node['y_position']+.5,node['z_position']+.1],
+                    position : [ x_origin , y_origin + .5 , z_origin + .1 ],
                     headline : node['title'],
                     description : node['description'],
                 })
@@ -67,34 +64,29 @@ $(document).ready(function(){
                 if (images.length>0){
                     for (var i in images){
                         const writing_texture = new THREE.TextureLoader(loadingManager).load(images[i]["image"]);
-                        const writing_pattern = new THREE.MeshBasicMaterial( { map: writing_texture } );
-                        writing_pattern.transparent = true;
+                        const writing_pattern = new THREE.MeshBasicMaterial( {
+                            map: writing_texture,
+                            transparent: true } );
                         const writingMesh = new THREE.Mesh(ar_display,writing_pattern);
-                        const z = z_origin - i * squishy
                         writingMesh.userData.markerID=int;
+                        
+                        const z = z_origin - i * node['squishy']
                         writingMesh.position.set(x_origin,y_origin,z);
+
                         scene.add(writingMesh);
                     }
                 } else{
                     const defaultMesh = new THREE.Mesh(defaultGeometry, defaultMaterial);
                     defaultMesh.userData.markerID=int;
+                    
                     defaultMesh.position.set(x_origin,y_origin,z_origin);
+                    
                     scene.add(defaultMesh);
-                    curtains_off();
                 }
     
             }
     
         } // end loadOrbs
-    
-        loadOrbs();
-
-        function curtains_off() {
-            $('#home_loader').css("animation","curtain_off 1s ease 1s 1 forwards")
-            $('#home_loader').on("animationend",function(e){
-                $('#home_loader').css("display","none");
-        })
-    }
 
         if (/Mobi|Android/i.test(navigator.userAgent)) {
             phoneScene();
@@ -105,13 +97,8 @@ $(document).ready(function(){
     } // end three_init
 
     function phoneScene(){
-        
+
         let controller, arrow, font;
-        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40);
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.xr.enabled = true;
-        $("#main_instructions").html("click to read");
-        $("#three_canvas").append( renderer.domElement );
 
         const fontLoader = new THREE.FontLoader()
         fontLoader.load(
@@ -120,7 +107,12 @@ $(document).ready(function(){
                 font = helvetiker;
             })
         
+        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.xr.enabled = true;
+        $("#three_canvas").append( renderer.domElement );
 
+        $("#main_instructions").html("click to read");
         document.body.appendChild(ARButton.createButton(renderer));
 
         arrow = new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 100, 0x47297B , 0,0);
@@ -129,11 +121,10 @@ $(document).ready(function(){
         function readPoem() {
 
             var intersects = raycaster.intersectObjects(scene.children);
-            var markerID = null;
             if(intersects.length){
                 for (var i = 0; i < intersects.length; i++)
                     if(intersects[i].object.userData.markerID){
-                        markerID = intersects[i].object.userData.markerID;
+                        var markerID = intersects[i].object.userData.markerID;
                         const titleGeometry = new THREE.TextGeometry(
                             markerData[markerID].headline,
                             {
@@ -276,12 +267,11 @@ $(document).ready(function(){
             $("#three_canvas").on('click', function(e){
                 raycaster.setFromCamera(mouse,camera);
                 var intersects = raycaster.intersectObjects(scene.children);
-                var markerID = null;
                 if(intersects.length){
                     $("#interaction_alerts #alert").html("");
                     for (var i = 0; i < intersects.length; i++)
                         if(intersects[i].object.userData.markerID){
-                            markerID = intersects[i].object.userData.markerID;
+                            var markerID = intersects[i].object.userData.markerID;
                             $("#course_content").addClass("open").removeClass("close");
                             $("#course_content .head").html(markerData[markerID].headline)
                             $("#course_content .body").html(markerData[markerID].description);
