211baf2cf5b29d98c6e7b9bd38fd7d6354e44c77
pabloguarneros <wrno.pablo@gmail.com>
2021-11-24 22:03:26 +0100

    Fixed line preventing compressor from finding the paint js file inside the static directory.

--- static/atlahua/js/paint/paint.js
+++ static/atlahua/js/paint/paint.js
@@ -1,6 +1,6 @@
 import $ from "jquery"
 import { loadCamera } from "../load_camera.js"
-import { ThreeCanvas } from "../three/create_environment.js"
+import { ThreeCanvas } from "./create_environment.js"
 
 $(document).ready(function(){
     const three_canvas = new ThreeCanvas();
