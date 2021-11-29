052cafe94dc3bf9bc524581935150ca65d7a444a
pabloguarneros <wrno.pablo@gmail.com>
2021-11-29 18:27:02 +0100

    New instructions hovering on Elsewhere.

--- static/atlahua/js/three/access_btn.js
+++ static/atlahua/js/three/access_btn.js
@@ -4,6 +4,12 @@ function character_has_loaded(){
     $("#enter_3D_world").addClass("reveal");
     $("#enter_3D_world").on('click',function(){
         $("#loading_page").addClass("fade");
+        setTimeout(function() {
+            document.getElementById('beginning_instructions_bg').classList.add("disappear");
+        }, 14500);
+        setTimeout(function() {
+            document.getElementById('beginning_instructions_bg').classList.add("move_right");
+        }, 5500);
         $("#loading_page").on("animationend",function(){
             $("#loading_page").css("display","none");
         })
